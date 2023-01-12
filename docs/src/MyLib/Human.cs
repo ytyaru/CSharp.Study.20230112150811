d2246b63b8285cb609dc5cf27b48a4ca420cdab8
ytyaru <yttry0@gmail.com>
2023-01-12 19:13:33 +0900

    クラスライブラリ追加

---
+++ docs/src/MyLib/Human.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLib;
+public class Human
+{
+    public string Name { get; init; } = "名無しの権兵衛";
+    public int Age { get; init; }
+    public bool Valid() {
+        if (this.Name.Length < 1 || 16 < this.Name.Length) { throw new ArgumentOutOfRangeException("名前は1〜16字以内であれ。"); }
+        if (this.Age < 0 || 255 < this.Age) { throw new ArgumentOutOfRangeException("年齢は0〜255以内であれ。"); }
+        return true;
+    }
+}
